4f8c5dc8c360cdb1e4ade233b61ab9b1080bf733
carrothead <Loczek11>
2019-11-28 19:39:53 +0100

    added Review & Recommendation ..ServiceApplicationTests

--- microservices/recommendation-service/src/test/java/com/geborskimateusz/microservices/core/recommendation/RecommendationServiceApplicationTests.java
+++ microservices/recommendation-service/src/test/java/com/geborskimateusz/microservices/core/recommendation/RecommendationServiceApplicationTests.java
@@ -2,15 +2,54 @@ package com.geborskimateusz.microservices.core.recommendation;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.reactive.server.WebTestClient;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class RecommendationServiceApplicationTests {
 
-	@Test
-	public void contextLoads() {
+    @Autowired
+    WebTestClient webTestClient;
+
+    @Test
+    public void contextLoads() {
+    }
+
+    @Test
+    public void getRecommendations() {
+        int given = 1;
+
+        webTestClient.get()
+                .uri("/recommendation?movieId=" + given)
+                .accept(MediaType.APPLICATION_JSON_UTF8)
+                .exchange()
+                .expectStatus().isOk()
+                .expectHeader().contentType(MediaType.APPLICATION_JSON_UTF8)
+                .expectBody()
+                .jsonPath("$.length()").isEqualTo(3)
+                .jsonPath("$[0].movieId").isEqualTo(given);
+
+    }
+
+    @Test
+    public void getRecommendationsThrowsInvalidInputException() {
+		int given = 0;
+
+		webTestClient.get()
+				.uri("/recommendation?movieId=" + given)
+				.accept(MediaType.APPLICATION_JSON_UTF8)
+				.exchange()
+				.expectStatus().isEqualTo(HttpStatus.UNPROCESSABLE_ENTITY)
+				.expectHeader().contentType(MediaType.APPLICATION_JSON_UTF8)
+				.expectBody()
+				.jsonPath("$.message").isEqualTo("Invalid productId: " + given);
+
+
 	}
 
 }
--- microservices/review-service/src/test/java/com/geborskimateusz/microservices/core/review/ReviewServiceApplicationTests.java
+++ microservices/review-service/src/test/java/com/geborskimateusz/microservices/core/review/ReviewServiceApplicationTests.java
@@ -2,15 +2,48 @@ package com.geborskimateusz.microservices.core.review;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.reactive.server.WebTestClient;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class ReviewServiceApplicationTests {
 
-	@Test
-	public void contextLoads() {
-	}
+    @Autowired
+    WebTestClient webTestClient;
+
+    @Test
+    public void contextLoads() {
+    }
+
+    @Test
+    public void getReviews() {
+        int given = 1;
+
+        webTestClient.get()
+                .uri("/review?movieId=" + given)
+                .accept(MediaType.APPLICATION_JSON_UTF8)
+                .exchange()
+                .expectStatus().isOk()
+                .expectHeader().contentType(MediaType.APPLICATION_JSON_UTF8)
+                .expectBody()
+                .jsonPath("$.length()").isEqualTo(3);
+    }
+
+    @Test
+    public void getReviewsThrowsInvalidInputException() {
+		int given = 0;
+
+		webTestClient.get()
+				.uri("/review?movieId=" + given)
+				.accept(MediaType.APPLICATION_JSON_UTF8)
+				.exchange()
+				.expectStatus().isEqualTo(HttpStatus.UNPROCESSABLE_ENTITY)
+				.expectHeader().contentType(MediaType.APPLICATION_JSON_UTF8);
+    }
 
 }
