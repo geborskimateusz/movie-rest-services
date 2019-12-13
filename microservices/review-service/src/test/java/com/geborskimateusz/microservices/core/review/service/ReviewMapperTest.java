3c52ca62c3f9d3a4d52007b30c9b56f8ca7f8399
carrothead <Loczek11>
2019-12-13 10:05:23 +0100

    added ReviewMapper tests

--- microservices/review-service/src/test/java/com/geborskimateusz/microservices/core/review/service/ReviewMapperTest.java
+++ microservices/review-service/src/test/java/com/geborskimateusz/microservices/core/review/service/ReviewMapperTest.java
@@ -4,6 +4,12 @@ import com.geborskimateusz.api.core.review.Review;
 import com.geborskimateusz.microservices.core.review.persistence.ReviewEntity;
 import org.junit.jupiter.api.Test;
 
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.MatcherAssert.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
 class ReviewMapperTest {
@@ -32,15 +38,42 @@ class ReviewMapperTest {
 
     @Test
     void apiToEntity() {
-        
+        Review api = getApi();
+        ReviewEntity entity = mapper.apiToEntity(api);
+
+        assertAll(() -> {
+            assertEquals(api.getReviewId(), (int) entity.getReviewId());
+            assertEquals(api.getMovieId(), (int) entity.getMovieId());
+            assertNotNull(api.getServiceAddress());
+        });
     }
 
     @Test
     void entityListToApiList() {
+        List<ReviewEntity> reviewEntities = Collections.singletonList(getEntity());
+        List<Review> reviews = mapper.entityListToApiList(reviewEntities);
+
+        ReviewEntity entity = reviewEntities.get(0);
+        Review api = reviews.get(0);
+
+        assertThat(reviews.size(), equalTo(1));
+        assertEquals((int) entity.getId(), api.getReviewId());
+        assertEquals((int) entity.getMovieId(), api.getMovieId());
+        assertNull(api.getServiceAddress());
     }
 
     @Test
     void apiListToEntityList() {
+        List<Review> reviews = Collections.singletonList(getApi());
+        List<ReviewEntity> reviewEntities = mapper.apiListToEntityList(reviews);
+
+        Review api = reviews.get(0);
+        ReviewEntity entity = reviewEntities.get(0);
+
+
+        assertThat(reviewEntities.size(), equalTo(1));
+        assertNull(entity.getId());
+        assertNull(entity.getVersion());
     }
 
     private ReviewEntity getEntity() {
@@ -54,5 +87,17 @@ class ReviewMapperTest {
                 .serviceAddress(FAKE_SERVICE_ADDRESS)
                 .subject(FAKE_SUBJECT)
                 .build();
+    }
+
+    private Review getApi() {
+        return Review.builder()
+                .reviewId(REVIEW_ID)
+                .movieId(MOVIE_ID)
+                .author(FAKE_AUTHOR)
+                .content(FAKE_CONTENT)
+                .serviceAddress(FAKE_SERVICE_ADDRESS)
+                .subject(FAKE_SUBJECT)
+                .build();
+
     }
 }
