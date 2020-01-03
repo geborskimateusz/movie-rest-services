97a28e9f83784d0c059077d68e6a2b884996d4ca
carrothead <Loczek11>
2020-01-03 16:09:44 +0100

    added Scheduler config

--- microservices/review-service/src/main/java/com/geborskimateusz/microservices/core/review/ReviewServiceApplication.java
+++ microservices/review-service/src/main/java/com/geborskimateusz/microservices/core/review/ReviewServiceApplication.java
@@ -1,6 +1,7 @@
 package com.geborskimateusz.microservices.core.review;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
---
+++ microservices/review-service/src/main/java/com/geborskimateusz/microservices/core/review/config/SchedulerConfig.java
@@ -0,0 +1,30 @@
+package com.geborskimateusz.microservices.core.review.config;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import reactor.core.scheduler.Scheduler;
+import reactor.core.scheduler.Schedulers;
+
+import java.util.concurrent.Executor;
+import java.util.concurrent.Executors;
+
+@Slf4j
+@Configuration
+public class SchedulerConfig {
+
+    private final Integer connectionPoolSize;
+
+    public SchedulerConfig(@Value("${spring.datasource.maximum-pool-size:10}") Integer connectionPoolSize) {
+        this.connectionPoolSize = connectionPoolSize;
+    }
+
+
+    @Bean
+    public Scheduler jdbcScheduler() {
+        log.info("Creates a jdbcScheduler with connectionPoolSize = " + connectionPoolSize);
+        return Schedulers.fromExecutor(Executors.newFixedThreadPool(connectionPoolSize));
+    }
+
+}
