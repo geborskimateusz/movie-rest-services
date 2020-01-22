adbed01f9edc38c0c267f0aa46e89729d8b31aa3
carrothead <Loczek11>
2020-01-22 22:11:01 +0100

    workig on automated test

--- test-em-all.bash
+++ test-em-all.bash
@@ -6,69 +6,99 @@
 #
 : ${HOST=localhost}
 : ${PORT=8080}
+: ${MOV_ID_REVS_RECS=2}
+: ${MOV_ID_NOT_FOUND=14}
+: ${MOV_ID_NO_RECS=114}
+: ${MOV_ID_NO_REVS=214}
 
 function assertCurl() {
 
-  local expectedHttpCode=$1
-  local curlCmd="$2 -w \"%{http_code}\""
-  local result=$(eval $curlCmd)
-  local httpCode="${result:(-3)}"
-  RESPONSE='' && ((${#result} > 3)) && RESPONSE="${result%???}"
+    local expectedHttpCode=$1
+    local curlCmd="$2 -w \"%{http_code}\""
+    local result=$(eval $curlCmd)
+    local httpCode="${result:(-3)}"
+    RESPONSE='' && (( ${#result} > 3 )) && RESPONSE="${result%???}"
 
-  echo "assertCurl will call $curlCmd"
-
-  if [ "$httpCode" = "$expectedHttpCode" ]; then
-    if [ "$httpCode" = "200" ]; then
-      echo "Test OK (HTTP Code: $httpCode)"
+    if [ "$httpCode" = "$expectedHttpCode" ]
+    then
+        if [ "$httpCode" = "200" ]
+        then
+            echo "Test OK (HTTP Code: $httpCode)"
+        else
+            echo "Test OK (HTTP Code: $httpCode, $RESPONSE)"
+        fi
+        return 0
     else
-      echo "Test OK (HTTP Code: $httpCode, $RESPONSE)"
+        echo  "Test FAILED, EXPECTED HTTP Code: $expectedHttpCode, GOT: $httpCode, WILL ABORT!"
+        echo  "- Failing command: $curlCmd"
+        echo  "- Response Body: $RESPONSE"
+        return 1
     fi
-  else
-    echo "Test FAILED, EXPECTED HTTP Code: $expectedHttpCode, GOT: $httpCode, WILL ABORT!"
-    echo "- Failing command: $curlCmd"
-    echo "- Response Body: $RESPONSE"
-    exit 1
-  fi
 }
 
 function assertEqual() {
 
-  local expected=$1
-  local actual=$2
+    local expected=$1
+    local actual=$2
 
-  if [ "$actual" = "$expected" ]; then
-    echo "Test OK (actual value: $actual)"
-  else
-    echo "Test FAILED, EXPECTED VALUE: $expected, ACTUAL VALUE: $actual, WILL ABORT"
-    exit 1
-  fi
+    if [ "$actual" = "$expected" ]
+    then
+        echo "Test OK (actual value: $actual)"
+        return 0
+    else
+        echo "Test FAILED, EXPECTED VALUE: $expected, ACTUAL VALUE: $actual, WILL ABORT"
+        return 1
+    fi
 }
 
 function testUrl() {
-  url=$@
-  if curl $url -ks -f -o /dev/null; then
-    echo "Ok"
-    return 0
-  else
-    echo -n "not yet"
-    return 1
-  fi
+    url=$@
+    if $url -ks -f -o /dev/null
+    then
+          return 0
+    else
+          return 1
+    fi;
 }
 
 function waitForService() {
-  url=$@
-  echo -n "Wait for: $url... "
-  n=0
-  until testUrl $url; do
-    n=$((n + 1))
-    if [[ $n == 100 ]]; then
-      echo " Give up"
-      exit 1
-    else
-      sleep 6
-      echo -n ", retry #$n "
+    url=$@
+    echo -n "Wait for: $url... "
+    n=0
+    until testUrl $url
+    do
+        n=$((n + 1))
+        if [[ $n == 100 ]]
+        then
+            echo " Give up"
+            exit 1
+        else
+            sleep 6
+            echo -n ", retry #$n "
+        fi
+    done
+}
+
+function testCompositeCreated() {
+
+    # Expect that the Movie Composite for movieId $MOV_ID_REVS_RECS has been created with three recommendations and three reviews
+    if ! assertCurl 200 "curl http://$HOST:$PORT/movie-composite/$MOV_ID_REVS_RECS -s"
+    then
+        echo -n "FAIL"
+        return 1
     fi
-  done
+
+    set +e
+    assertEqual "$MOV_ID_REVS_RECS" $(echo $RESPONSE | jq .movieId)
+    if [ "$?" -eq "1" ] ; then return 1; fi
+
+    assertEqual 3 $(echo $RESPONSE | jq ".recommendations | length")
+    if [ "$?" -eq "1" ] ; then return 1; fi
+
+    assertEqual 3 $(echo $RESPONSE | jq ".reviews | length")
+    if [ "$?" -eq "1" ] ; then return 1; fi
+
+    set -e
 }
 
 function waitForMessageProcessing() {
@@ -97,21 +127,15 @@ function recreateComposite() {
   local movieId=$1
   local composite=$2
 
-  local url=http://$HOST:$PORT/movie-composite
-  eval echo "Will call:  $url"
-
-  echo "With body: "
-  echo "$composite"
-
-  curl -X POST http://$HOST:$PORT/movie-composite -H "Content-Type: application/json" --data "$composite"
-
+    assertCurl 200 "curl -X DELETE http://$HOST:$PORT/movie-composite/${movieId} -s"
+    curl -X POST http://$HOST:$PORT/movie-composite -H "Content-Type: application/json" --data "$composite"
 }
 
 function setupData() {
 
-  body='{
-    "genre": "Science Fiction",
-    "movieId": 1,
+  body="{\"movieId\":$MOV_ID_REVS_RECS"
+    body+=\
+',"genre": "Science Fiction",
     "title": "Star Wars 1",
     "recommendations": [
       {
@@ -144,9 +168,9 @@ function setupData() {
   }'
   recreateComposite 1 "$body"
 
-    body='{
-      "genre": "Science Fiction",
-      "movieId": 113,
+    body="{\"movieId\":$MOV_ID_NO_RECS"
+    body+=\
+',"genre": "Science Fiction",
       "title": "Star Wars 1",
       "reviews": [
         {
@@ -169,11 +193,11 @@ function setupData() {
         }
       ]
     }'
-    recreateComposite 113 "$body"
+    recreateComposite "$MOV_ID_NO_RECS" "$body"
 
-    body='{
-      "genre": "Science Fiction",
-      "movieId": 213,
+    body="{\"movieId\":$MOV_ID_NO_REVS"
+    body+=\
+',"genre": "Science Fiction",
       "title": "Star Wars 1",
       "recommendations": [
         {
@@ -196,71 +220,58 @@ function setupData() {
         }
       ]
     }'
-    recreateComposite 213 "$body"
+    recreateComposite "$MOV_ID_NO_REVS" "$body"
 }
+
 
 set -e
 
-echo "Start:" $(date)
+echo "Start Tests:" `date`
 
 echo "HOST=${HOST}"
 echo "PORT=${PORT}"
 
-if [[ $@ == *"start"* ]]; then
-  echo "Restarting the test environment..."
-  echo "$ docker-compose down"
-  docker-compose down
-
-  echo "$ mvn clean install"
-  mvn clean install
-
-  #echo "$ docker-compose build"
-  #docker-compose build
-
-  echo "$ docker-compose up -d"
-  docker-compose up -d
+if [[ $@ == *"start"* ]]
+then
+    echo "Restarting the test environment..."
+    echo "$ docker-compose down --remove-orphans"
+    docker-compose down --remove-orphans
+    echo "$ docker-compose up -d"
+    docker-compose up -d
 fi
 
-#waitForService curl -X DELETE http://$HOST:$PORT/movie-composite/13
 waitForService curl http://$HOST:$PORT/actuator/health
 
 setupData
 
 waitForMessageProcessing
 
-# Verify that a normal request works, expect three recommendations and on review
-assertCurl 200 "curl http://$HOST:$PORT/movie-composite/1 -s"
-assertEqual 1 $(echo $RESPONSE | jq .movieId)
+# Verify that a normal request works, expect three recommendations and three reviews
+assertCurl 200 "curl http://$HOST:$PORT/movie-composite/$MOV_ID_REVS_RECS -s"
+assertEqual "$MOV_ID_REVS_RECS" $(echo $RESPONSE | jq .movieId)
 assertEqual 3 $(echo $RESPONSE | jq ".recommendations | length")
-assertEqual 1 $(echo $RESPONSE | jq ".reviews | length")
+assertEqual 3 $(echo $RESPONSE | jq ".reviews | length")
 
-# Verify that a 404 (Not Found) error is returned for a non existing movieId (13)
-assertCurl 404 "curl http://$HOST:$PORT/movie-composite/13 -s"
+# Verify that a 404 (Not Found) error is returned for a non existing movieId ($MOV_ID_NOT_FOUND)
+assertCurl 404 "curl http://$HOST:$PORT/movie-composite/$MOV_ID_NOT_FOUND -s"
 
-# Verify that no recommendations are returned for movieId 113
-assertCurl 200 "curl http://$HOST:$PORT/movie-composite/113 -s"
-assertEqual 113 $(echo $RESPONSE | jq .movieId)
+# Verify that no recommendations are returned for movieId $MOV_ID_NO_RECS
+assertCurl 200 "curl http://$HOST:$PORT/movie-composite/$MOV_ID_NO_RECS -s"
+assertEqual "$MOV_ID_NO_RECS" $(echo $RESPONSE | jq .movieId)
 assertEqual 0 $(echo $RESPONSE | jq ".recommendations | length")
 assertEqual 3 $(echo $RESPONSE | jq ".reviews | length")
 
-# Verify that no reviews are returned for movieId 213
-assertCurl 200 "curl http://$HOST:$PORT/movie-composite/213 -s"
-assertEqual 213 $(echo $RESPONSE | jq .movieId)
+# Verify that no reviews are returned for movieId $MOV_ID_NO_REVS
+assertCurl 200 "curl http://$HOST:$PORT/movie-composite/$MOV_ID_NO_REVS -s"
+assertEqual $MOV_ID_NO_REVS $(echo $RESPONSE | jq .movieId)
 assertEqual 3 $(echo $RESPONSE | jq ".recommendations | length")
 assertEqual 0 $(echo $RESPONSE | jq ".reviews | length")
 
-# Verify that a 422 (Unprocessable Entity) error is returned for a movieId that is out of range (-1)
-assertCurl 422 "curl http://$HOST:$PORT/movie-composite/-1 -s"
-assertEqual "\"Invalid movieId: -1\"" "$(echo $RESPONSE | jq .message)"
+echo "End, all tests OK:" `date`
 
-# Verify that a 400 (Bad Request) error error is returned for a movieId that is not a number, i.e. invalid format
-assertCurl 400 "curl http://$HOST:$PORT/movie-composite/invalidProductId -s"
-assertEqual "\"Type mismatch.\"" "$(echo $RESPONSE | jq .message)"
-
-if [[ $@ == *"stop"* ]]; then
-  echo "We are done, stopping the test environment..."
-  echo "$ docker-compose down"
-  docker-compose down
+if [[ $@ == *"stop"* ]]
+then
+    echo "Stopping the test environment..."
+    echo "$ docker-compose down --remove-orphans"
+    docker-compose down --remove-orphans
 fi
-
-echo "End:" $(date)
