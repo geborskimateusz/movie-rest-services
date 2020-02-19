eb6b043190a131fd5965eabb70363ff6403d82ff
carrothead <Loczek11>
2020-02-19 18:45:41 +0100

    added authorization-server initializer

---
+++ spring-cloud/authorization-server/src/main/java/com/geborskimateusz/authorizationserver/AuthorizationServerApplication.java
@@ -0,0 +1,13 @@
+package com.geborskimateusz.authorizationserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthorizationServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthorizationServerApplication.class, args);
+	}
+
+}
