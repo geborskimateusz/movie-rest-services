d31297ea34fedef2a6f65c778eebb6c7561010e0
geborskimateusz <mateuszgeborski@gmail.com>
2020-03-24 19:32:34 +0100

    cleaning up

--- spring-cloud/config-server/src/main/java/com/geborskimateusz/configserver/SecurityConfig.java
+++ spring-cloud/config-server/src/main/java/com/geborskimateusz/configserver/SecurityConfig.java
@@ -10,10 +10,11 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
-                .csrf().disable()
+
+                .csrf()
+                .disable()
                 .authorizeRequests()
-                .anyRequest()
-                .authenticated()
+                .anyRequest().authenticated()
                 .and()
                 .httpBasic();
     }
